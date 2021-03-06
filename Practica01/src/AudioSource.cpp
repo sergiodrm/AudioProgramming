b5771529603212dbeaa18c97354bf633dd7adcd0
sergiodrm <47793261+sergiodrm@users.noreply.github.com>
2021-03-06 13:05:01 +0100

    Audio listener implemented

--- Practica01/src/AudioSource.cpp
+++ Practica01/src/AudioSource.cpp
@@ -1,31 +1,46 @@
 #include "AudioSource.h"
 
+
+#include "AudioBuffer.h"
 #include "AL/al.h"
 
 CAudioSource::CAudioSource(CAudioBuffer* _audioBuffer)
-  : m_audioBuffer(_audioBuffer)
 {
   alGenSources(1, &m_audioSourceBuffer);
   alSourcef(m_audioSourceBuffer, AL_PITCH, 1.f);
   alSourcef(m_audioSourceBuffer, AL_GAIN, 1.f);
   alSource3f(m_audioSourceBuffer, AL_POSITION, 0.f, 0.f, 0.f);
   alSource3f(m_audioSourceBuffer, AL_VELOCITY, 0.f, 0.f, 0.f);
+  alSourcei(m_audioSourceBuffer, AL_BUFFER, _audioBuffer->GetALBuffer());
 }
 
-void CAudioSource::SetPitch(float _pitch) {}
+CAudioSource::~CAudioSource()
+{
+  alDeleteSources(1, &m_audioSourceBuffer);
+}
 
-void CAudioSource::SetGain(float _gain) {}
+void CAudioSource::SetPitch(float _pitch)                     { alSourcef(m_audioSourceBuffer, AL_PITCH, _pitch); }
+void CAudioSource::SetGain(float _gain)                       { alSourcef(m_audioSourceBuffer, AL_GAIN, _gain); }
+void CAudioSource::SetLooping(bool _loop)                     { alSourcei(m_audioSourceBuffer, AL_LOOPING, _loop ? 1 : 0); }
+void CAudioSource::SetPosition(float _x, float _y, float _z)  { alSource3f(m_audioSourceBuffer, AL_POSITION, _x, _y, _z); }
+void CAudioSource::SetVelocity(float _x, float _y, float _z)  { alSource3f(m_audioSourceBuffer, AL_VELOCITY, _x, _y, _z); }
 
-void CAudioSource::SetLooping(bool _loop) {}
+void CAudioSource::Play()
+{
+  alSourcePlay(m_audioSourceBuffer);
+  m_bPlaying = true;
+}
 
-void CAudioSource::SetPosition(float _x, float _y, float _z) {}
+void CAudioSource::Stop()
+{
+  alSourceStop(m_audioSourceBuffer);
+  m_bPlaying = false;
+}
 
-void CAudioSource::SetVelocity(float _x, float _y, float _z) {}
+void CAudioSource::Pause()
+{
+  alSourcePause(m_audioSourceBuffer);
+  m_bPlaying = false;
+}
 
-void CAudioSource::Play() {}
-
-void CAudioSource::Stop() {}
-
-void CAudioSource::Pause() {}
-
-bool CAudioSource::IsPlaying() const {}
+bool CAudioSource::IsPlaying() const { return m_bPlaying; }
--- Practica01/src/AudioSource.h
+++ Practica01/src/AudioSource.h
@@ -10,6 +10,7 @@ class CAudioSource
 public:
 
   CAudioSource(CAudioBuffer* _audioBuffer);
+  ~CAudioSource();
 
 public:
 
@@ -26,7 +27,7 @@ public:
 
 private:
 
-  CAudioBuffer* m_audioBuffer;
+  bool m_bPlaying;
   uint32_t m_audioSourceBuffer;
 };
 
